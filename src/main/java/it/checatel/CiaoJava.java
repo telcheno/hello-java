666a82c8b86186ea20c27f02dfb120bcae922032
Telmo Checa <telmocheca1@gmail.com>
2024-04-20 09:58:00 +0200

    primo commiti di java

---
+++ src/main/java/it/checatel/CiaoJava.java
@@ -0,0 +1,11 @@
+package it.checatel;
+
+public class CiaoJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Caio Java!");
+		System.out.println("Hello World!");
+	}
+
+}
