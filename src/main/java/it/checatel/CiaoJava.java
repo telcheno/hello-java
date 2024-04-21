1ce100244c626cfb1fcde921fe4d99cd7c7137de
Telmo Checa <telmocheca1@gmail.com>
2024-04-21 17:22:48 +0200

    ADD: tipi di dati in java

--- src/main/java/it/checatel/CiaoJava.java
+++ src/main/java/it/checatel/CiaoJava.java
@@ -6,6 +6,21 @@ public class CiaoJava {
 		// TODO Auto-generated method stub
 		System.out.println("Caio Java!");
 		System.out.println("Hello World!");
+		
+		//dati primitivi in java
+		
+		int mioIntero = 2147483647;
+		long mioLong = 9223372036854775807L;
+		float mioFloat = 5.5F;
+		byte mioByte = -10;
+		short mioShort = 32000;
+		double mioDouble = 123124132.2131231231D;
+		char mioChar = 'C';
+		boolean mioBoolean = true;
+		
+		//dati non primitivi
+		
+		String variabileStringa = "Ciao io sono una stringa";
 	}
 
 }
